8e658da2770b1020ca8b3d60ef0f024e84e4c161
Zundal <ajs15010120@gmail.com>
2022-06-02 17:46:23 +0900

    first

---
+++ src/test/java/com/example/secondjavaproject/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secondjavaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
